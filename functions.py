b608504dc7053880beac0571d43819b176894fdc
jacobmh98 <jacobmelchiorhansen98@gmail.com>
2023-05-20 11:24:51 +0200

    version 2

--- functions.py
+++ functions.py
@@ -3,6 +3,7 @@ import numpy as np
 import cv2
 from point_conversion import PI, inv_PI
 import random
+import math
 
 def compute_camera_matrix(f, dx, dy, alpha=1, beta=1):
     """ Compute the camera matrix """
@@ -10,7 +11,7 @@ def compute_camera_matrix(f, dx, dy, alpha=1, beta=1):
                   [0, alpha*f, dy],
                   [0, 0, 1]])
     return A
-def test():
+def test1():
     A = compute_camera_matrix(2774.5, 806.8, 622.6, 1, 0)
     print(A)
 
@@ -20,7 +21,7 @@ def compute_projection_matrix(f, dx, dy, alpha, beta, R, t):
     P = A @ np.hstack((R, t))
 
     return P
-def test():
+def test2():
     R = cv2.Rodrigues(np.array([0.1, -0.2, -0.1]))[0]
     t = np.array([[0.03], [0.06], [-0.02]])
     f = 350
@@ -50,12 +51,115 @@ def match_SIFT_keypoints(im1, im2):
     matches = bf.match(des1, des2)
     return matches
 
+def scaleSpaced(im, sigma, n):
+    """ Computing the scale space pyramid of an image """
+    blurred_imgs = []
 
-def hest(Q1, Q2):
-    """ Estimate a homography given two sets of matching points"""
+    for i in range(n):
+        # Compute the Gaussian kernel given the value of sigma
+        sigma_i = sigma * 2**i
+        kernel_size = int(6 * sigma-i)  # Determine kernel size based on sigma
+        kernel = np.empty((kernel_size,kernel_size))
+
+        for x in range(kernel.shape[0]):
+            for y in range(kernel.shape[1]):
+                kernel[x, y] = (1/(2 * np.pi * sigma_i**2)) * np.exp(-(x**2 + y**2) / (2 * sigma_i**2))
+
+        # Applying the Gaussian kernel to the image 
+        blurred_im = cv2.filter2D(im, -1, kernel)
+        blurred_imgs.append(blurred_im)
+
+    return blurred_imgs
+
+def differenceOfGaussian(im, sigma, n):
+    """ Compute the difference of gaussians for an image given sigma"""
+    DoG = []
+    im_scales = scaleSpaced(im, sigma, n)  # Obtain the scale space pyramid using the previous function
+
+    for i in range(n - 1):
+        dog = im_scales[i] - im_scales[i + 1]
+        DoG.append(dog)
+    
+    return DoG
+def test3():
+    im = cv2.imread('week8_data/sunflowers.jpg') # Loading the image 
+    im = im.astype(float).mean(2)/255 # Converting image to black and white and floating point
+    #im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    sigma = 2
+    n = 4
+    #scaleSpace = scaleSpaced(im, sigma, n)
+    DoG = differenceOfGaussian(im, sigma, n)
+
+    for d in DoG:
+        plt.imshow(d)
+        plt.show()
+
+def convert_line_to_homogeneous_coordinates(a, b, c):
+    """ Convert a line on the form ax + by = c to homogeneous coordinates """
+    return np.array([a, b, -c]).reshape((3,1))
+
+def determine_if_point_on_line(l, p):
+    """ Determine if a point is on a line in homogeneous coordinates """
+    return l @ p == 0
+
+def determine_intersection_of_lines(l0, l1):
+    """ Given to lines in homogeneous coordinates determines their point of intersection """
+    return np.cross(l0, l1)
+
+def determine_distance_from_point_to_line(l, p):
+    """ Computes the distance from a point to a line (both in homogeneous coordinates)"""
+    return abs(l @ p) / np.sqrt(l[0] ** 2 + l[1] ** 2)
+
+def project_points_distortion(f, alpha, beta, dx, dy, R, t, distCoeffs, P):
+    """ Project a 3D point to 2D with distortion """
+    A_p = np.array([[f, 0, 0],
+                    [0, f, 0],
+                    [0, 0, 1]])
+    
+    A_q = np.array([[1, beta, dx],
+                    [0, alpha, dy],
+                    [0, 0, 1]])
+    
+    Rt = np.hstack((R, t))
+
+    pd = inv_PI(PI(A_p @ Rt @ P)) # distorted projection coordinates in 2D homogeneous coordinates
+
+    dr = 0 # computing the radial distortion
+    r = np.sqrt(pd[0]**2 + pd[1]**2)
+    for i, k in enumerate(distCoeffs):
+        dr += k * (r ** ((i + 1)*2))
+    
+    pc = inv_PI(pd[0:2] * (1 + dr)) # corrected projection coordinates in 2D homogeneous coordinates 
+    
+    q = A_q @ pc # projected point with radial distortion in homogeneous coordinates
+    return q
+def test4():
+    R = np.array([[0.9887, -0.0004, 0.1500],
+                  [0.0008, 1.0000, -0.0030],
+                  [-0.1500, 0.0031, 0.9887]])
+    t = np.array([-2.1811, 0.0399, 0.5072]).reshape((3,1))
+    f = 2774.5
+    dx = 806.8
+    dy = 622.6
+    alpha = 1
+    beta = 0
+
+    distCoeffs = np.array([-5.1806e-8, 1.4192e-15])
+
+    Q = np.array([-1.3540, 0.5631, 8.8734]).reshape((3,1))
+
+    q = project_points_distortion(f, alpha, beta, dx, dy, R, t, distCoeffs, inv_PI(Q))
+    print(q)
+
+def map_points_using_homography(P, H):
+    """ Map a set of 2D inhomogeneous points using a homography"""
+
+    P_hom = inv_PI(P)
+    return PI(H @ P_hom)
+
+def hest_linear(Q1, Q2):
+    """ Estimate a homography given two sets of matching points using the lienar algorithm """
     n = len(Q1[0])
-
-    B = np.zeros((3*n, 9))
 
     for i in range(n):
         x1i = Q1[0, i]
@@ -63,87 +167,46 @@ def hest(Q1, Q2):
         x2i = Q2[0, i]
         y2i = Q2[1, i]
 
-        bi_T = np.array([[0, -x2i, x2i*y1i, 0, -y2i, y2i*y1i, 0, -1, y1i],
+        if i == 0:
+            B = np.array([[0, -x2i, x2i*y1i, 0, -y2i, y2i*y1i, 0, -1, y1i],
                          [x2i, 0, -x2i*x1i, y2i, 0, -y2i*x1i, 1, 0, -x1i],
                          [-x2i*y1i, x2i*x1i, 0, -y2i*y1i, y2i*x1i, 0, -y1i, x1i, 0]])
-
-        B[i:(i+3), :] = bi_T
+        else:
+            bi_T = np.array([[0, -x2i, x2i*y1i, 0, -y2i, y2i*y1i, 0, -1, y1i],
+                         [x2i, 0, -x2i*x1i, y2i, 0, -y2i*x1i, 1, 0, -x1i],
+                         [-x2i*y1i, x2i*x1i, 0, -y2i*y1i, y2i*x1i, 0, -y1i, x1i, 0]])
+            
+            B = np.vstack((B, bi_T))
+        
     _, _, V_T = np.linalg.svd(B,)
     return V_T[-1].reshape((3,3), order='F')
 
-def estHomographyRANSAC(Q1, Q2, iterations):
-    """ Compute the homography using RANSAC """
-    n = len(Q1[0])
-    errors = np.empty(iterations)
-    homographies = np.empty(iterations)
-    inliers_im1 = []
-    inliers_im2 = []
+def normalize_points(pts):
+    """ Normalize a set of 2D homogeneous points so they have mean 0 and std 1 """
 
-    for it in range(iterations):
-        # Selecting 4 random points from Q1 and Q2
-        i1, i2, i3, i4 = random.sample(range(0, n-1), 4)
-        Q1_points = np.hstack((Q1[:, i1].reshape((2, 1)), Q1[:, i2].reshape((2, 1)), Q1[:, i3].reshape((2, 1)), Q1[:, i4].reshape((2, 1))))
-        Q2_points = np.hstack((Q2[:, i1].reshape((2, 1)), Q2[:, i2].reshape((2, 1)), Q2[:, i3].reshape((2, 1)), Q2[:, i4].reshape((2, 1))))
-        
-        # Estimating the homography for the four points
-        H = hest(Q1_points, Q2_points)
-        print("det H=" + str(np.linalg.det(H)))
+    mean = np.mean(pts, axis=1)
+    std = np.std(pts, axis=1)
 
-        # Computing the error for the estimated points
-        distances = np.empty((4))
-        for i in range(4):
-            print(Q1[:,i].reshape((2,1)))
-            p1i = inv_PI(Q1[:, i].reshape((2,1)))
-            p2i = inv_PI(Q2[:, i].reshape((2,1)))
-
-            dist = np.linalg.norm(PI(H @ p2i) - PI(p1i))**2 + np.linalg.norm(PI(np.linalg.inv(H) @ p1i) - PI(p2i))**2
-            distances[i] = dist
-
-            sigma = 3
-            threshold = np.sqrt(3.84 * sigma**2)
-            
-            if dist < threshold:
-                inliers_im1.append(p1i)
-                inliers_im2.append(p2i)
-        
-        error = np.mean(distances)
-        
-        # Saving the homography and error
-        homographies[it] = H
-        errors[it] = error
-
-    print(len(inliers_im1))
-    # Select the homography with the lowest error
-    min_error = np.argmin(errors)
-    return homographies[min_error]
-
-def test():
-    im1 = cv2.imread('data/im1.jpg')
-    im2 = cv2.imread('data/im2.jpg')
-    kp1, kp2, des1, des2 = find_SIFT_keypoints_descriptors(im1, im2)
+    T_inv = np.array([[std[0], 0, mean[0]],
+                      [0, std[1], mean[1]],
+                      [0, 0, 1]])
     
-    matches = match_SIFT_keypoints(im1, im2)
-    n = len(matches)
+    T = np.linalg.inv(T_inv)
+    return T, T @ pts
+
+def test5():
+    P = np.array([[1, 0, 2, 2],
+                  [1, 3, 3, 4]])
     
-    Q1 = np.empty((2, n))
-    Q2 = np.empty((2, n))
-
-    for i, match in enumerate(matches):
-
-        query_idx = match.queryIdx  # Index of the keypoint in the source set
-        train_idx = match.trainIdx  # Index of the keypoint in the destination set
-
-        query_point = kp1[query_idx].pt  # Keypoint coordinates in the source set
-        train_point = kp2[train_idx].pt  # Keypoint coordinates in the destination set
-
-        Q1[0,i] = query_point[0]
-        Q1[1,i] = query_point[1]
+    H = np.array([[-2, 0, 1],
+                  [1, -2, 0],
+                  [0, 0, 3]])
     
-        Q1[0,i] = train_point[0]
-        Q1[1,i] = train_point[1]
+    Q = map_points_using_homography(P, H)
 
-    estHomographyRANSAC(Q1, Q2, 200)
+    H_est = hest_linear(Q, P)
+    scale = H[0,0] / H_est[0,0]
+    print(np.round(H_est * (scale)))
 
-    #out = cv2.drawMatches(im1, kp1, im2, kp2, matches[:1], None, flags=2)
-    #plt.imshow(out[:,:,::-1]), plt.show()
-test()
+    normalize_points(P)
+test5()
